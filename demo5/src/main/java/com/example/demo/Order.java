c7a13c0bc6abaf481bb10e2d1804f19b6dbea8b3
boomer_19 <80147820+gari950@users.noreply.github.com>
2023-07-01 23:22:23 +0530

    Create Order.java

---
+++ demo5/src/main/java/com/example/demo/Order.java
@@ -0,0 +1,45 @@
+package com.example.demo5;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @Column(name = "orderid", nullable = false)
+    private Integer id;
+
+    @Column(name = "username", length = 50)
+    private String username;
+
+    @Column(name = "offerid")
+    private Integer offerid;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getOfferid() {
+        return offerid;
+    }
+
+    public void setOfferid(Integer offerid) {
+        this.offerid = offerid;
+    }
+
+}
